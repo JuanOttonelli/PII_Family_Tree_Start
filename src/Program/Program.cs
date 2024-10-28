aa14f52dd7e2b3e3bda15ade56cd09413534ae2f
Manuel <manuel.chouhy@correo.ucu.edu.uy>
2024-10-28 18:19:44 -0300

    Manuel

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,25 +9,40 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            
+            Person abuelo = new Person("Carlos", "Gomez", DateOnly.Parse("1945-04-10"));
+            Person padre = new Person("Luis", "Gomez", DateOnly.Parse("1975-08-22"));
+            Person hijo1 = new Person("Juan", "Gomez", DateOnly.Parse("2000-07-13"));
+            Person hijo2 = new Person("Ana", "Gomez", DateOnly.Parse("2003-12-15"));
+            Person nieto1 = new Person("Manolo", "Gomez", DateOnly.Parse("2023-11-25"));
+            Person nieto2 = new Person("Juancito", "Gomez", DateOnly.Parse("2024-01-18"));
+            Person nieto3 = new Person("Pepe", "Gomez", DateOnly.Parse("2022-09-10"));
+            
+            Node<Person> nodoAbuelo = new Node<Person>(abuelo);
+            Node<Person> nodoPadre = new Node<Person>(padre);
+            Node<Person> nodoHijo1 = new Node<Person>(hijo1);
+            Node<Person> nodoHijo2 = new Node<Person>(hijo2);
+            Node<Person> nodoNieto1 = new Node<Person>(nieto1);
+            Node<Person> nodoNieto2 = new Node<Person>(nieto2);
+            Node<Person> nodoNieto3 = new Node<Person>(nieto3);
 
-            n1.AddChildren(n2);
-            n1.AddChildren(n3);
+            nodoAbuelo.AddChildren(nodoPadre);
+            nodoPadre.AddChildren(nodoHijo1);
+            nodoPadre.AddChildren(nodoHijo2);
+            nodoHijo1.AddChildren(nodoNieto1);
+            nodoHijo1.AddChildren(nodoNieto2);
+            nodoHijo2.AddChildren(nodoNieto3);
+            MostrarArbol(nodoAbuelo, "");
 
-            n2.AddChildren(n4);
-            n2.AddChildren(n5);
-
-            n3.AddChildren(n6);
-            n3.AddChildren(n7);
-            Person persona1 = new Person("Juan", "Ottonelli", DateOnly.Parse("13/07/2002"));
-            Console.WriteLine(persona1.Age);
-            // visitar el árbol aquí
+        }
+        static void MostrarArbol(Node<Person> nodo, string indentacion)
+        {
+            if (nodo == null) return;
+            Console.WriteLine($"{indentacion}- {nodo.Data.FullName}, {nodo.Data.Age} años");
+            foreach (var hijo in nodo.Children)
+            {
+                MostrarArbol(hijo, indentacion + "   ");
+            }
         }
     }
 }
