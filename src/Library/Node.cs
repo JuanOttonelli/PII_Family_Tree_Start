268493b56aac0dbc21fec18fb1aba5bb787f39fd
Manuel <manuel.chouhy@correo.ucu.edu.uy>
2024-10-28 10:40:56 -0300

    MANUEL

--- src/Library/Node.cs
+++ src/Library/Node.cs
@@ -37,19 +37,3 @@ namespace Library
         
     }
 }
-public class Person
-{
-    public string Nombre { get; set; }
-    public int Edad { get; set; }
-
-    public Person(string nombre, int edad)
-    {
-        Nombre = nombre;
-        Edad = edad;
-    }
-
-    public override string ToString()
-    {
-        return $"{Nombre}, {Edad} años";
-    }
-}
